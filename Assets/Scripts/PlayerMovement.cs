aa127e86a72a28f25434f1198ef4343f0df8562d
Mantas Banevičius <61312268+Helzinko@users.noreply.github.com>
2022-01-23 14:57:25 +0200

    Implemented Input System | Player movement + rotation

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private CharacterController _controller;
+
+    public float _playerMovementSpeed = 10.0f;
+
+    private Vector3 inputVector;
+    void Start()
+    {
+        inputVector = Vector2.zero;
+        _controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        _controller.SimpleMove(inputVector * _playerMovementSpeed);
+    }
+
+    public void OnMovement(InputAction.CallbackContext value)
+    {
+        var inputValue = value.ReadValue<Vector2>();
+        inputVector = new Vector3(inputValue.x, 0, inputValue.y);
+    }
+
+    public void OnAim(InputAction.CallbackContext value)
+    {
+        var inputValue = value.ReadValue<Vector2>();
+
+        if (inputValue.x != 0.0f || inputValue.y != 0.0f) {
+            var angle = Mathf.Atan2(inputValue.y, -inputValue.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.AngleAxis(angle, Vector3.up);
+        }
+    }
+}
